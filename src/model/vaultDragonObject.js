de88bc04cb10cb9d68cc89d14194a07c314cafef
Ernst Pierre <pitrens.studio@gmail.com>
2017-09-13 10:33:19 +0800

    Final touch

--- src/model/vaultDragonObject.js
+++ src/model/vaultDragonObject.js
@@ -5,7 +5,7 @@ let Schema = mongoose.Schema;
 let vaultDragonObjectSchema = new Schema({
 	key   : String ,
 	value : String , 
-	time  :  {type:Number, default: new Date().getTime()} 
+	time  :  {type:Number, default: new Date().getTime()/1000} 
 
 });
 
